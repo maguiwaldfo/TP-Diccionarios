37d1d934052d8cb2c27c134770df6b1d23c3ecb2
MAGALI WALDFOGIEL <49553387@ort>
2026-03-26 08:42:04 -0300

    Creacion del metodo Agregar Recurso

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dictionary<string , int> dicRecursos = new Dictionary<string , int>();
+
+        AgregarRecurso("madera", 40 , dicRecursos);
+        AgregarRecurso("hierro" , 80 , dicRecursos);
+        AgregarRecurso("Soga" , 10 , dicRecursos); 
+    }
+    public void AgregarRecurso(string recurso , int cantidad , Dictionary<string , int> dicRecursos)
+    {
+        bool esta = dicRecursos.ContainsKey(recurso); 
+        if (esta == false)
+        {
+            dicRecursos.Add(recurso , cantidad)
+        }
+        else 
+        {
+            Console.WriteLine("Recurso existente")
+        }
     }
 }
